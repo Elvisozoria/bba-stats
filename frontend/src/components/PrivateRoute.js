43b211ab20014e8ee1cb905b25908ef54e3de927
Elvis Ozoria <elvis.ozoria@gmail.com>
2024-10-09 16:58:20 -0400

    remove comment

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('token'); // Suponiendo que guardas el token en localStorage
+  const token = localStorage.getItem('token'); 
 
   return token ? children : <Navigate to="/login" />;
 };
